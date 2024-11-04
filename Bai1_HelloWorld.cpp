c6491f951bec0b00971b4253d3f120a1b79f4df5
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-04 11:23:54 +0700

    Add project files.

---
+++ Bai1_HelloWorld.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "Toi ten la: " << "\tNguyen Phu Thanh\n" << endl;
+    cout << "em chuc thay mai dinh, mai dinh!" << "\tchuc thay 8386! XD";
+    return 0;
+}
